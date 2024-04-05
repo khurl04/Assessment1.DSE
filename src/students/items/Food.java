02b8e37b666071a6e583e9f28c5c3b2e9035e25e
VergaraKhurl <163851438+VergaraKhurl@users.noreply.github.com>
2024-04-05 19:35:50 +1030

    Just finished coding the Food Class

--- src/students/items/Food.java
+++ src/students/items/Food.java
@@ -1,6 +1,11 @@
 package students.items;
 
 public class Food {
+    public Food(int maturationAge, int deathAge, double monetaryValue) {
+        super(maturationAge, deathAge, monetaryValue);
+    }
 
+    @Override
+    public abstract String toString();
 
 }
