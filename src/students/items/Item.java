b2e89f146f2d2608517bdb0e42a6676978d284d2
VergaraKhurl <163851438+VergaraKhurl@users.noreply.github.com>
2024-04-05 19:33:45 +1030

    I just did the Item Class

--- src/students/items/Item.java
+++ src/students/items/Item.java
@@ -1,6 +1,50 @@
 package students.items;
 
 public class Item {
+	private int age;
+    private int maturationAge;
+    private int deathAge;
+    private double monetaryValue;
 
+    public Item(int maturationAge, int deathAge, double monetaryValue) {
+        this.age = 0;
+        this.maturationAge = maturationAge;
+        this.deathAge = deathAge;
+        this.monetaryValue = monetaryValue;
+    }
+
+    public void tick() {
+        age++;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean died() {
+        return age > deathAge;
+    }
+
+    public double getValue() {
+        if (age >= maturationAge)
+            return monetaryValue;
+        else
+            return 0; // Returns 0 if not matured yet
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this)
+            return true;
+        if (!(obj instanceof Item))
+            return false;
+        Item other = (Item) obj;
+        return this.age == other.age &&
+               this.deathAge == other.deathAge &&
+               this.maturationAge == other.maturationAge &&
+               this.monetaryValue == other.monetaryValue;
+    }
+
+    public abstract String toString();
 	
 }
