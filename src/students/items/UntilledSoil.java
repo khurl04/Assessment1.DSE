8ff8d9e8e8041b643f3531d2a3d37342ce830d5f
VergaraKhurl <163851438+VergaraKhurl@users.noreply.github.com>
2024-04-05 19:43:21 +1030

    finished in untilledSoil class

--- src/students/items/UntilledSoil.java
+++ src/students/items/UntilledSoil.java
@@ -1,6 +1,21 @@
 package students.items;
 
 public class UntilledSoil {
+	
+    private static final int MATURATION_AGE = Integer.MAX_VALUE; 
+    private static final int DEATH_AGE = Integer.MAX_VALUE; 
+    private static final double MONETARY_VALUE = -1; 
+
+   
+    public UntilledSoil() {
+        super(MATURATION_AGE, DEATH_AGE, MONETARY_VALUE);
+    }
+
+    
+    @Override
+    public String toString() {
+        return "/"; 
+    }
 
 
 }
