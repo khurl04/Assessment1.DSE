a2fcff097c70069d355231cbc27599481b49e742
VergaraKhurl <163851438+VergaraKhurl@users.noreply.github.com>
2024-04-05 19:44:43 +1030

    finished with soil class

--- src/students/items/Soil.java
+++ src/students/items/Soil.java
@@ -1,5 +1,17 @@
 package students.items;
 
 public class Soil {
+	private static final int MATURATION_AGE = Integer.MAX_VALUE;
+    private static final int DEATH_AGE = Integer.MAX_VALUE; 
+    private static final double MONETARY_VALUE = 0; 
 
+    public Soil() {
+        super(MATURATION_AGE, DEATH_AGE, MONETARY_VALUE);
+    }
+
+
+    @Override
+    public String toString() {
+        return ".";
+    }
 }
