cc8e497deba47a4990e75a78128269a35c6c7128
VergaraKhurl <163851438+VergaraKhurl@users.noreply.github.com>
2024-04-05 19:37:45 +1030

    finished coding the Grain Class

--- src/students/items/Grain.java
+++ src/students/items/Grain.java
@@ -1,6 +1,37 @@
 package students.items;
 
 public class Grain{
+	 private static final int MATURATION_AGE = 2;
+	    private static final int DEATH_AGE = 6;
+	    private static final double MONETARY_VALUE = 2;
+	    private static final double BUY_COST = 1;
 
+	    private static int generationCount = 0;
+
+	   
+	    public Grain() {
+	        super(MATURATION_AGE, DEATH_AGE, MONETARY_VALUE);
+	        generationCount++; 
+	    }
+
+	
+	    public static int getGenerationCount() {
+	        return generationCount;
+	    }
+
+	 
+	    public static double getBuyCost() {
+	        return BUY_COST;
+	    }
+
+	    
+	    @Override
+	    public String toString() {
+	        if (getAge() < getMaturationAge()) {
+	            return "g"; 
+	        } else {
+	            return "G"; 
+	        }
+	    }
 	
 }
