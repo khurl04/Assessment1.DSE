4f6fa13d8be8ebbd0501fa829b57fae22a7875d8
VergaraKhurl <163851438+VergaraKhurl@users.noreply.github.com>
2024-04-05 19:42:19 +1030

    finished the weed class

--- src/students/items/Weed.java
+++ src/students/items/Weed.java
@@ -3,4 +3,19 @@ package students.items;
 public class Weed
 {
 
+    private static final int MATURATION_AGE = Integer.MAX_VALUE; 
+    private static final int DEATH_AGE = Integer.MAX_VALUE; 
+    private static final double MONETARY_VALUE = -1; 
+
+   
+    public Weed() {
+        super(MATURATION_AGE, DEATH_AGE, MONETARY_VALUE);
+    }
+
+    
+    @Override
+    public String toString() {
+        return "#"; 
+    }
+
 }
