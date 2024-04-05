f217aec0d5fc2cf7d540c699fe609ffabb7ea6dc
VergaraKhurl <163851438+VergaraKhurl@users.noreply.github.com>
2024-04-05 19:39:42 +1030

    finshied with Apples Class

--- src/students/items/Apples.java
+++ src/students/items/Apples.java
@@ -1,5 +1,36 @@
 package students.items;
 
 public class Apples {
+	private static final int MATURATION_AGE = 3;
+    private static final int DEATH_AGE = 5;
+    private static final double MONETARY_VALUE = 3;
+    private static final double BUY_COST = 2;
 
+    private static int generationCount = 0;
+
+   
+    public Apples() {
+        super(MATURATION_AGE, DEATH_AGE, MONETARY_VALUE);
+        generationCount++; 
+    }
+
+    
+    public static int getGenerationCount() {
+        return generationCount;
+    }
+
+   
+    public static double getBuyCost() {
+        return BUY_COST;
+    }
+
+ 
+    @Override
+    public String toString() {
+        if (getAge() < getMaturationAge()) {
+            return "a";
+        } else {
+            return "A";
+        }
+    }
 }
